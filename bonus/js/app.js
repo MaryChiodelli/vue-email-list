025a52068b1ee9d4218ea3850833e0787ff30a7e
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-01 17:14:38 +0200

    refactoring

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -1,15 +1,21 @@
 const app = new Vue({
     el: '#app',
     data: {
+        num: 10,
         mailingList: []
     },
-    mounted() {
-        for (let i = 0; i < 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((res) => {
-                    this.mailingList.push(res.data.response);
-                });
+    methods: {
+        fetchMail(num) {
+            for (let i = 0; i < num; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        this.mailingList.push(res.data.response);
+                    });
+            }
         }
+    },
+    beforeMount() {
+        this.fetchMail(this.num);
     }
 });
 
